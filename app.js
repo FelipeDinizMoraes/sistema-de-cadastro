8aa625f2101d1fe2ea78836aa83dced0839332f6
DMFelipe88 <diniz.felipe.moraes@gmail.com>
2023-01-26 09:59:17 -0300

    Adicionando um console log

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-console.log "rodando o sistema de gereciamento de jogos"
-console.log "aplicação está se conectando ao banco de dados"
+console.log ("rodando o sistema de gereciamento de jogos")
+console.log ("aplicação está se conectando ao banco de dados")
+// Mais um console log de teste:
+console.log ("O Paulo fez esse commit sem você ver na aula")
+console.log ("Enviando para o GitHub")
+
